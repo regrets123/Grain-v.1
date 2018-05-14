d26dce099a1517c9a6090d29d67988c21e8876d4
KazzTruul <kazztruul@hotmail.com>
2018-05-14 15:40:02 +0200

    scenfix

--- Assets/Scripts/Menu_Scripts/LoadingManager.cs
+++ Assets/Scripts/Menu_Scripts/LoadingManager.cs
@@ -7,7 +7,8 @@ using UnityEngine.UI;
 /* By Johanna Pettersson
  * Följande kod är lånad från: <https://www.youtube.com/watch?v=YMj2qPq9CP8&t=350s> */
 
-public class LoadingManager : MonoBehaviour {
+public class LoadingManager : MonoBehaviour
+{
 
     [SerializeField]
     Slider progressBar;
@@ -29,14 +30,18 @@ public class LoadingManager : MonoBehaviour {
         set;
     }
 
-    public void LoadScene (string scene)
+    public void LoadScene(string scene)
     {
         StartCoroutine(LoadingScene(scene));
     }
 
-    IEnumerator LoadingScene (string scene)
+    IEnumerator LoadingScene(string scene)
     {
-        AsyncOperation operation = SceneManager.LoadSceneAsync(scene);
+        AsyncOperation operation;
+        if (scene == "Master Scene 1 - Managers")
+            operation = SceneManager.LoadSceneAsync(scene);
+        else
+            operation = SceneManager.LoadSceneAsync(scene, LoadSceneMode.Additive);
 
         loadingScreen.SetActive(true);
 
