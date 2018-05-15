ce62ef361b4e56299da8e23a9e829c1b690a8690
TacoBellDoge <aforandreas@hotmail.com>
2018-05-15 14:54:32 +0200

    Fireball projectile A okay

--- Assets/Scripts/Ability_Scripts/MagicProjectile.cs
+++ Assets/Scripts/Ability_Scripts/MagicProjectile.cs
@@ -9,23 +9,36 @@ public class MagicProjectile : BaseAbilityScript
     [SerializeField]
     GameObject magicProjectilePrefab;
     
-    [SerializeField]
-    Camera cameraPosition;
-
     //A currently public float, 
     //so you can adjust the speed as necessary.
 
     [SerializeField]
     float speed = 20.0f;
 
+    Vector3 dir;
+
     public override void UseAbility()
     {
         base.UseAbility();
         //instantiate a magic projectile
-        GameObject magicProjectile = Instantiate(magicProjectilePrefab, transform.position, transform.rotation);
+        GameObject magicProjectile = Instantiate
+            (
+                magicProjectilePrefab, transform.position, new Quaternion(camera.transform.rotation.x, movement.transform.rotation.y, camera.transform.rotation.z, movement.transform.rotation.w)
+            );
 
         abilities.Anim.SetTrigger("MagicAttack");
+
+        if (!camFollow.LockOn)
+        {
+            dir = magicProjectile.transform.forward + Vector3.up;
+
+        }
+        else if (camFollow.LockOn)
+        {
+            dir = ((camFollow.LookAtMe.transform.position - magicProjectile.transform.position)/10) + Vector3.up;
+        }
+
         //Add a force to the magic going forward form your current position.
-        magicProjectile.GetComponent<Rigidbody>().AddForce(abilities.transform.forward * speed, ForceMode.Impulse);
+        magicProjectile.GetComponent<Rigidbody>().AddForce(dir * speed, ForceMode.Impulse);
     }
 }
